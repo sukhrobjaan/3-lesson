125f408799c7dafa4a928d697f31cf331e1c9955
Suxrob <choriyorovsuhrob@gmail.com>
2024-01-13 00:13:55 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import example_2.App;
+import example_2.Divice;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/example_2/App.java
@@ -0,0 +1,27 @@
+package example_2;
+
+public class App {
+    String appName;
+    int appVolume;
+
+    public App(String appName, int appVolume) {
+        this.appName = appName;
+        this.appVolume = appVolume;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public int getAppVolume() {
+        return appVolume;
+    }
+
+    public void setAppVolume(int appVolume) {
+        this.appVolume = appVolume;
+    }
+}
